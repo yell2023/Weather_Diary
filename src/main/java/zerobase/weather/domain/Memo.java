737ee876712bd2553ada1f26efb1305a68740764
Yang ye lin <oky07031217@gmail.com>
2024-01-10 11:13:28 +0900

    Feat:파일 초기 세팅

---
+++ src/main/java/zerobase/weather/domain/Memo.java
@@ -0,0 +1,15 @@
+package zerobase.weather.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Memo {
+    private int id;
+    private String text;
+}
